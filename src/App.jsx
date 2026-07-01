8219212ac1ad5793b2f9c34745da763bb509413c
sugi1410 <suganthi1410@gmail.com>
2026-07-01 15:11:45 -0500

    added css and modified App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-
+import FeedBackForm from './components/FeedBackForm'
+import './App.css'
 
 function App() {
-
+  
 
   return (
-    <>
-      
-    </>
-  )
+    <FeedBackForm />
+  );
 }
 
-export default App
+export default App;
